b3a04a50af89f7a4bcf411d7e1b2d09a721839d8
hardman0218 <59394004+hardman0218@users.noreply.github.com>
2022-12-12 23:54:35 +0900

    画像の位置を修正

--- chatbot.js
+++ chatbot.js
@@ -1,9 +1,12 @@
-let string=document[0].forms[0];
-console.log(string);
-function btn(arg) {
-    /*string=arg;*/
-    console.log(string);
-    console.log(11);
+/*let string=document[0].forms[0];
+console.log(string);*/
+
+function btn(event.form.inpput.value) {
+    /*debugger*/
+    event.preventDefault();
+    console.log(event.form.inpput.value);
+        /*string=arg;*/
+/*    console.log(string);*/
     alert("Hello world!!");
 }
 function Push(id){
