1851f342dc651b6111da7a3b920d787ebad02575
hardman0218 <59394004+hardman0218@users.noreply.github.com>
2022-12-12 23:24:18 +0900

    23:24

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,11 @@
-function ShowAlart() {
+let string=document[0].forms[0];
+console.log(string);
+function btn(arg) {
+    /*string=arg;*/
+    console.log(string);
+    console.log(11);
     alert("Hello world!!");
 }
-    
+function Push(id){
+
+}
